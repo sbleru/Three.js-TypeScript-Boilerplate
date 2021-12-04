0d8554c6336678c67933170747804eed735df0ec
sbleru <usareru@gmail.com>
2021-12-04 16:58:55 +0900

    chore: Add comment

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,24 +3,38 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
+/** 
+ * Prepare camera.
+ */
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
+/**
+ * Prepare render element.
+ */
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+/**
+ * Create orbit control
+ */
 const controls = new OrbitControls(camera, renderer.domElement)
 
+/**
+ * Create model to render.
+ */
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
-
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+/**
+ * Re-render if window resized.
+ */
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -29,6 +43,9 @@ function onWindowResize() {
     render()
 }
 
+/**
+ * Animate and render.
+ */
 function animate() {
     requestAnimationFrame(animate)
 
@@ -40,7 +57,14 @@ function animate() {
     render()
 }
 
+/**
+ * Render scene.
+ */
 function render() {
     renderer.render(scene, camera)
 }
+
+/**
+ * Trigger a world.
+ */
 animate()
